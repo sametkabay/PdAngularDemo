1eeeb505329cb3a31f1f683a136257081f57b3d3
Samet Kabay <smtkby@hotmail.com>
2018-04-10 18:48:48 +0300

    loginModuleConf

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
-import { LoginRoutes } from './Login.routing';
+import { LoginRoutes } from './login.routing';
 
 @NgModule({
     imports: [
